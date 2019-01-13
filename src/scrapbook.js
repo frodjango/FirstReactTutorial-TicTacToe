af0bc5571d9895f0da3a4eb678fb4a8f40b6753a
Francois Rousseau <frodjango@Francoiss-MacBook-Pro-2.local>
2019-01-12 20:11:43 -0500

    Ajuot d'un guide de l'usager pour le fichier scrapbook.js

--- src/scrapbook.js
+++ src/scrapbook.js
@@ -1,3 +1,13 @@
+// Guide de l'usager sur MacBook
+//
+// Francoiss-MacBook-Pro-2:src frodjango$ pwd
+// /Users/frodjango/code/React/my-app/src
+// Francoiss-MacBook-Pro-2:src frodjango$ node scrapbook.js 
+// 0 1
+// 1 2
+// 2 0
+// 3 3
+
 let history = [1, 2, 0, 3];
 
 moves = history.map((_element, _index) => {
